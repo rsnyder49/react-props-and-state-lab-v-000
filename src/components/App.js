8fe24bf47abc74988c0fb8ac83c18f15b5cd039b
Robert Snyder <r.snyder49@gmail.com>
2019-01-11 23:32:04 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,30 @@ class App extends React.Component {
       }
     }
   }
+  
+  onChangeType = ({target: {value}}) => {
+    this.setState({
+      filters: { ...this.state.filters, type: value } 
+    });
+  }
+
+   onFindPetsClick = () => {
+    let apiURL = '/api/pets'
+    if (this.state.filters.type !== 'all') {apiURL += `?type=${this.state.filters.type}`}
+    fetch(apiURL)
+    .then(res => res.json())
+    .then(pets => this.setState({ pets }));
+  }
+
+   onAdoptPet = (petId) => {
+    const pets = this.state.pets.map(pet => {
+      return (pet.id === petId) ? { ...pet, isAdopted: true } : pet;
+    });
+    this.setState({
+      pets
+    });
+  }
+
 
   render() {
     return (
@@ -24,10 +48,12 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} 
+            onFindPetsClick={this.onFindPetsClick} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} 
+            onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
